24ee47ecf8a4ed3d07319fe44ae63d7b3d4ef850
Philomina Peter <CS5919@cloudium.local>
2021-09-15 15:38:12 +0530

    file

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-console.log("Hello World");
+console.log("Hello world");
 
-
-
-
-
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+ 
 
 
 
