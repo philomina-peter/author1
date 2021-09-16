39521fdb6009f5bd5b6f28d4ddca8992eeb8e398
Philomina Peter <CS5919@cloudium.local>
2021-09-16 11:30:24 +0530

    query

--- myApp.js
+++ myApp.js
@@ -41,6 +41,16 @@ app.get("/:word/echo", (req, res) => {
       echo: word
     });
   });
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
 
 
 
