9985af3e42ab06fecb8ef2123873630f65fdb649
Philomina Peter <CS5919@cloudium.local>
2021-09-16 09:02:37 +0530

    route

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ console.log("Hello world");
 app.use(express.static(__dirname + "/public"));
 
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", (req, res) =>){
+    res.json({
+        message: "Hello json"
+    });
+});
 
 
 
