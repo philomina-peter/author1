f21483b94917921b9baba62dfefdd208b2530a12
Philomina Peter <CS5919@cloudium.local>
2021-09-16 11:42:30 +0530

    bodyparser

--- myApp.js
+++ myApp.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser=require('body-parser');
 console.log("Hello world");
 app.use(function middleware(req,res,next) 
     {
         console.log(req.method + " "+ req.path + " - "+ req.ip);
         next();
     }); 
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/public"));
 app.get("/", function(req, res) {
